a9a564c1183f75b4a59f6f054698f86fa66dabd2
Byron Denham <byron@byrons-mbp.ddns.uark.edu>
2022-02-15 11:05:37 -0600

    Face Recognition functionality added (still issues with drawing to screen)

--- src/main.py
+++ src/main.py
@@ -24,15 +24,50 @@ def getFrame(myDrone,w,h):
     return img
 
 
+face_cascade = cv.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 myDrone = intitialize()
 
 while True:
-    #print("Battery:",myDrone.get_battery())
+#    print("test")
+#    print("Battery:",myDrone.get_battery())
     #Frame reading
-    #img = getFrame(myDrone,w,h)
+    img = getFrame(myDrone,w,h)
+
+
+    cap = myDrone.get_video_capture()
+
+    height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+
+    # Calculate frame center
+    center_x = int(width/2)
+    center_y = int(height/2)
+
+    # Draw the center of the frame
+    cv.circle(img, (center_x, center_y), 10, (0, 255, 0))
+
+
+
+    # Convert frame to grayscale in order to apply the haar cascade
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, minNeighbors=5)
+
+    # If a face is recognized, draw a rectangle over it and add it to the face list
+    face_center_x = center_x
+    face_center_y = center_y
+    z_area = 0
+    for face in faces:
+        (x, y, w, h) = face
+#        cv.rectangle(img,(x, y),(x + w, y + h),(255, 255, 0), 2)
+
+        face_center_x = x + int(h/2)
+        face_center_y = y + int(w/2)
+        z_area = w * h
+
+        cv.circle(img, (face_center_x, face_center_y), 10, (0, 0, 255))
 
     #Show frame in window
-    #cv.imshow('Frame',img)
+    cv.imshow('Frame',img)
     if cv.waitKey(1) & 0xFF == ord('q'):
         print("Battery:",myDrone.get_battery())
         myDrone.land()
