c2bbb137a534470708e34ac6914310b654416e7b
Byron Denham <byron@byrons-mbp.ddns.uark.edu>
2022-03-03 15:29:14 -0600

    face recognition with adjusted drawing to screen

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from djitellopy import Tello
 import cv2 as cv
 import numpy as np
 
-w,h = 360,240
+SCREEN_WIDTH=720
+SCREEN_HEIGHT=480
 
 def intitialize():
     myDrone = Tello()
@@ -17,7 +18,7 @@ def intitialize():
     myDrone.streamon()
     return myDrone
 
-def getFrame(myDrone,w,h):
+def getFrame(myDrone,w=SCREEN_WIDTH,h=SCREEN_HEIGHT):
     myFrame = myDrone.get_frame_read()
     myFrame = myFrame.frame
     img = cv.resize(myFrame,(w,h))
@@ -28,27 +29,24 @@ face_cascade = cv.CascadeClassifier('cascades/haarcascade_frontalface_default.xm
 myDrone = intitialize()
 
 while True:
-#    print("test")
-#    print("Battery:",myDrone.get_battery())
+
     #Frame reading
-    img = getFrame(myDrone,w,h)
+    img = getFrame(myDrone)
 
 
     cap = myDrone.get_video_capture()
 
-    height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
-    width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 
-    # Calculate frame center
-    center_x = int(width/2)
-    center_y = int(height/2)
+    # Calculate center of frame
+    center_x = int(SCREEN_WIDTH/2)
+    center_y = int(SCREEN_HEIGHT/2)
 
-    # Draw the center of the frame
+    # Draw circle at center of the frame
     cv.circle(img, (center_x, center_y), 10, (0, 255, 0))
 
 
 
-    # Convert frame to grayscale in order to apply the haar cascade
+    # Convert frame to grayscale in order to apply the haar cascade for face identification
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, minNeighbors=5)
 
@@ -58,13 +56,18 @@ while True:
     z_area = 0
     for face in faces:
         (x, y, w, h) = face
-#        cv.rectangle(img,(x, y),(x + w, y + h),(255, 255, 0), 2)
+        cv.rectangle(img,(x, y),(x + w, y + h),(255, 255, 0), 2)
 
         face_center_x = x + int(h/2)
         face_center_y = y + int(w/2)
         z_area = w * h
-
         cv.circle(img, (face_center_x, face_center_y), 10, (0, 0, 255))
+
+    # Calculate recognized face offset from center
+    offset_x = face_center_x - center_x
+    offset_y = face_center_y - center_y
+
+    cv.putText(img, f'[{offset_x}, {offset_y}, {z_area}]', (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv.LINE_AA)
 
     #Show frame in window
     cv.imshow('Frame',img)
