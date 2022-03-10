17ef602e9e73a75bf1021bc81ebc6d58e222f048
nullbett <59373037+nullbett@users.noreply.github.com>
2022-03-10 10:25:49 -0600

    integration between controller code and face detection

--- src/main.py
+++ src/main.py
@@ -1,138 +1,194 @@
-from djitellopy import Tello
-import cv2 as cv
+from djidronepy import Tello
+import cv2 
+import pygame
 import numpy as np
-import pygame as pg
 import time
 
-#Width and height of image window
-w,h = 360,240
+# Speed of the drone
+
 S = 60
+# Frames per second of the pygame window display
+# A low number also results in input lag, as input information is processed once per frame.
+
 FPS = 120
-send_rc_control = False
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 720
+face_cascade = cv2.CascadeClassifier('./src/cascades/haarcascade_frontalface_default.xml')
 
-#Creates a drone object
-def intitialize():
-    drone = Tello()
-    drone.connect()
+class FrontEnd(object):
+    """ Maintains the Tello display and moves it through the keyboard keys.
+        Press escape key to quit.
+        The controls are:
+            - T: Takeoff
+            - L: Land
+            - Arrow keys: Forward, backward, left and right.
+            - A and D: Counter clockwise and clockwise rotations (yaw)
+            - W and S: Up and down.
+    """
 
-    #Creates pg object
-    pg.init()
-
-    drone.for_back_velocity = 0
-    drone.left_right_velocity = 0
-    drone.up_down_velocity = 0
-    drone.yaw_velocity = 0
-    drone.speed = 10
-    
-    pg.time.set_timer(pg.USEREVENT + 1, 1000 // FPS)
-    #print("Battery:",drone.get_battery())
-    drone.set_speed(drone.speed)
-    drone.streamoff()
-    drone.streamon()
-    return drone
-
-#Builds the frame from the drone camera
-def getFrame(drone,w,h):
-    #myFrame = drone.get_frame_read()
-    #myFrame = myFrame.frame
-    #img = cv.resize(myFrame,(w,h))
-    return img
-
-def keydown(drone,key):
-       
-    if key == pg.K_UP:  # set forward velocity
-        drone.for_back_velocity = S
-    elif key == pg.K_DOWN:  # set backward velocity
-        drone.for_back_velocity = -S
-    elif key == pg.K_LEFT:  # set left velocity
-        drone.left_right_velocity = -S
-    elif key == pg.K_RIGHT:  # set right velocity
-        drone.left_right_velocity = S
-    elif key == pg.K_w:  # set up velocity
-        drone.up_down_velocity = S
-    elif key == pg.K_s:  # set down velocity
-        drone.up_down_velocity = -S
-    elif key == pg.K_a:  # set yaw counter clockwise velocity
-        drone.yaw_velocity = -S
-    elif key == pg.K_d:  # set yaw clockwise velocity
-        drone.yaw_velocity = S
-
-def keyup(drone, key):
+    def __init__(self):
+        # Init pygame
         
-    if key == pg.K_UP or key == pg.K_DOWN:  # set zero forward/backward velocity
-        drone.for_back_velocity = 0
-    elif key == pg.K_LEFT or key == pg.K_RIGHT:  # set zero left/right velocity
-        drone.left_right_velocity = 0
-    elif key == pg.K_w or key == pg.K_s:  # set zero up/down velocity
-        drone.up_down_velocity = 0
-    elif key == pg.K_a or key == pg.K_d:  # set zero yaw velocity
-        drone.yaw_velocity = 0
-    elif key == pg.K_t:  # takeoff
-        drone.takeoff()
-        send_rc_control = True
-    elif key == pg.K_l:  # land
-        not drone.land()
-        send_rc_control = False
+        pygame.init()
 
-def update(drone):
-    if send_rc_control:
-        drone.send_rc_control(drone.left_right_velocity, drone.for_back_velocity,drone.up_down_velocity, drone.yaw_velocity)
+        
+
+        # Creat pygame window
+        pygame.display.set_caption("Automatic Drone Tracking")
+        self.screen = pygame.display.set_mode([960, 720])
+
+        # Init Tello object that interacts with the Tello drone
+        
+        self.drone = Tello()
+
+        # Drone velocities between -100~100
+        
+        self.for_back_velocity = 0
+        self.left_right_velocity = 0
+        self.up_down_velocity = 0
+        self.yaw_velocity = 0
+        self.speed = 10
+
+        self.send_rc_control = False
+
+        # create update timer
+        pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
+
+    def run(self):
+
+        self.drone.connect()
+        self.drone.set_speed(self.speed)
+
+        # In case streaming is on. This happens when we quit this program without the escape key.
+        self.drone.streamoff()
+        self.drone.streamon()
+
+        frame_read = self.drone.get_frame_read()
+
+        should_stop = False
+        while not should_stop:
+
+            for event in pygame.event.get():
+                if event.type == pygame.USEREVENT + 1:
+                    self.update()
+                elif event.type == pygame.QUIT:
+                    should_stop = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        should_stop = True
+                    else:
+                        self.keydown(event.key)
+                elif event.type == pygame.KEYUP:
+                    self.keyup(event.key)
+
+            if frame_read.stopped:
+                break
+
+            ##Insert face detections -Byron
+            self.screen.fill([0, 0, 0])
+            #----------------------------
+            frame = frame_read.frame
+            frame = cv2.resize(frame, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+            # Calculate center of frame
+            center_x = int(SCREEN_WIDTH/2)
+            center_y = int(SCREEN_HEIGHT/2)
+            # Draw circle at center of the frame
+            cv2.circle(frame, (center_x, center_y), 10, (0, 255, 0))
+
+            # Convert frame to grayscale in order to apply the haar cascade for face identification
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, minNeighbors=5)
+
+            # If a face is recognized, draw a rectangle over it and add it to the face list
+            face_center_x = center_x
+            face_center_y = center_y
+            z_area = 0
+            for face in faces:
+                (x, y, w, h) = face
+                cv2.rectangle(frame,(x, y),(x + w, y + h),(255, 255, 0), 2)
+
+                face_center_x = x + int(h/2)
+                face_center_y = y + int(w/2)
+                z_area = w * h
+                cv2.circle(frame, (face_center_x, face_center_y), 10, (0, 0, 255))
+
+            # Calculate recognized face offset from center
+            offset_x = face_center_x - center_x
+            offset_y = face_center_y - center_y
+
+            cv2.putText(frame, f'[{offset_x}, {offset_y}, {z_area}]', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+            #----------------------------
+            # battery n. 电池
+            text = "Battery: {}%".format(self.drone.get_battery())
+            cv2.putText(frame, text, (5, 720 - 5),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = np.rot90(frame)
+            frame = np.flipud(frame)
+
+            
+
+            frame = pygame.surfarray.make_surface(frame)
+            self.screen.blit(frame, (0, 0))
+            pygame.display.update()
+
+            time.sleep(1 / FPS)
+
+        # Call it always before finishing. To deallocate resources.
+        self.drone.end()
+
+    def keydown(self, key):
+        """ Update velocities based on key pressed
+        """
+        if key == pygame.K_UP:  # set forward velocity
+            self.for_back_velocity = S
+        elif key == pygame.K_DOWN:  # set backward velocity
+            self.for_back_velocity = -S
+        elif key == pygame.K_LEFT:  # set left velocity
+            self.left_right_velocity = -S
+        elif key == pygame.K_RIGHT:  # set right velocity
+            self.left_right_velocity = S
+        elif key == pygame.K_w:  # set up velocity
+            self.up_down_velocity = S
+        elif key == pygame.K_s:  # set down velocity
+            self.up_down_velocity = -S
+        elif key == pygame.K_a:  # set yaw counter clockwise velocity
+            self.yaw_velocity = -S
+        elif key == pygame.K_d:  # set yaw clockwise velocity
+            self.yaw_velocity = S
+
+    def keyup(self, key):
+        """ Update velocities based on key released
+        """
+        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
+            self.for_back_velocity = 0
+        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
+            self.left_right_velocity = 0
+        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
+            self.up_down_velocity = 0
+        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
+            self.yaw_velocity = 0
+        elif key == pygame.K_t:  # takeoff
+            self.drone.takeoff()
+            self.send_rc_control = True
+        elif key == pygame.K_l:  # land
+            not self.drone.land()
+            self.send_rc_control = False
+
+    def update(self):
+        """ Update routine. Send velocities to Tello.
+        """
+        if self.send_rc_control:
+            self.drone.send_rc_control(self.left_right_velocity, self.for_back_velocity,self.up_down_velocity, self.yaw_velocity)
 
 
-#Invokes drone initialization
-drone = intitialize()
-myFrame = drone.get_frame_read()
+def main():
+    frontend = FrontEnd()
 
-pg.display.set_caption("Video")
-screen = pg.display.set_mode([960,720])
+    # run frontend
 
-#Main loop
-loop = True
-while loop:
-
-    for event in pg.event.get():
-        if event.type == pg.USEREVENT + 1:
-            update(drone)
-        elif event.type == pg.QUIT:
-            should_stop = True
-        elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_ESCAPE:
-                loop = True
-            else:
-                keydown(drone,event.key)
-        elif event.type == pg.KEYUP:
-            keyup(drone, event.key)
-
-        # if frame_read.stopped:
-        #     break
-
-    myFrame = myFrame.frame
-    screen.fill([0,0,0])
-
-    
-    text = "Battery: {}%".format(drone.get_battery())
-    cv.putText(myFrame, text, (5, 720 - 5),cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    myFrame = cv.cvtColor(myFrame, cv.COLOR_BGR2RGB)
-    myFrame = np.rot90(myFrame)
-    myFrame = np.flipud(myFrame)
-
-    myFrame = pg.surfarray.make_surface(myFrame)
-    screen.blit(myFrame, (0, 0))
-    pg.display.update()
-    #Invokes frame method to create an image
-    #img = getFrame(drone,w,h)
-
-    #Show image in window
-    #cv.imshow('Frame',img)
-
-    #cv.waitKey(1)
-    #Exit the program by pressing 'q'
-    # if keyboard.is_pressed('q'):
-    #     print("Battery:",drone.get_battery())
-    #     drone.land()
-    #     break
-
-    time.sleep(1 / FPS)
+    frontend.run()
 
 
-
+if __name__ == '__main__':
+    main()
