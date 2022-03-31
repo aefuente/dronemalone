890b91f60065bdecc5e984d1562be5d652306e1a
parke <parkersamuelweber@gmail.com>
2022-03-31 17:09:43 -0500

    updating FSM files

---
+++ fsm/expertFSM.py
@@ -0,0 +1,147 @@
+from pickle import TRUE
+
+
+# INIT = 'INIT'
+# TAKEOFF = 'TAKEOFF'
+# LAND = 'LAND'
+# IDLE = 'IDLE'
+# MOVE_LEFT = 'MOVE_LEFT'
+# MOVE_RIGHT = 'MOVE_RIGHT'
+# MOVE_DOWN = 'MOVE_DOWN'
+# MOVE_UP = 'MOVE_UP'
+# MOVE_FORWARD = 'MOVE_FORWARD'
+# MOVE_BACKWARD = 'MOVE_BACKWARD'
+# X = 100
+# PrevX = 0
+# Y = 0
+# PrevY = 0
+# ROI = 0
+# PrevROI = 0
+# next_state = 1
+
+next_state = 1
+
+def INIT():
+    print("INIT STATE")
+    global next_state
+    next_state = 2 # move to TAKEOFF STATE
+    command = ""
+    return command
+
+def TAKEOFF():
+    print("TAKEOFF_STATE")
+    global next_state
+    next_state = 4 # move to idle state
+    command = "takeoff"
+    return command
+    
+
+def LAND():
+    print("LAND_STATE")
+    command = "land"
+    return command
+    
+# To-Do: check buffer/thresholds (20 at the moment). Or replace with ABS value percentage comparisons
+def IDLE():
+    print("IDLE_STATE")
+    global next_state, X, Y, ROI
+    if X < PrevX - 20:
+        next_state = 5 #MOVE_LEFT #5
+    elif X > PrevX + 20:
+        next_state = 6 #MOVE_RIGHT #6
+    elif ROI > PrevROI:
+        next_state = 10 # MOVE_BACKWARD #10
+    elif ROI < PrevROI:
+        next_state = 9 #MOVE_FORWARD #9
+    elif Y < PrevY - 20:
+        next_state = 8 #MOVE_UP #8
+    elif Y > PrevY + 20:
+        next_state = 7 #MOVE_DOWN #7
+    else:
+        next_state = 4
+        
+	#elif(battery < battery_shutoff):
+	#	next_state = FINAL
+    command = ""
+    return command
+
+def MOVE_LEFT():
+    print("MOVE_LEFT_STATE")
+    global next_state
+    next_state = 4
+    command = "left 20"
+    return command
+    
+
+def MOVE_RIGHT():
+    print("MOVE_RIGHT_STATE")
+    global next_state
+    next_state = 4
+    command = "right 20"
+    return command
+    
+
+def MOVE_DOWN():
+    print("MOVE_DOWN_STATE")
+    global next_state
+    next_state = 4
+    command = "down 20"
+    return command
+
+
+def MOVE_UP():
+    print("MOVE_UP_STATE")
+    global next_state
+    next_state = 4
+    command = "up 20"
+    return command
+
+def MOVE_FORWARD():
+    print("MOVE_FORWARD_STATE")
+    global next_state
+    next_state = 4
+    command = "forward 20"
+    return command
+
+def MOVE_BACKWARD():
+    print("MOVE_BACKWARD_STATE")
+    global next_state
+    next_state = 4
+    command = "back 20"
+    return command
+
+states = {
+    1: INIT, 
+    2: TAKEOFF, 
+    3: LAND, 
+    4: IDLE, 
+    5: MOVE_LEFT, 
+    6: MOVE_RIGHT,
+    7: MOVE_DOWN, 
+    8: MOVE_UP, 
+    9: MOVE_FORWARD, 
+    10: MOVE_BACKWARD
+    }
+
+def FSM_TICK(update_X, update_Y, update_ROI): # Main FSM call
+    global X, Y, ROI, PrevX, PrevY, PrevROI, next_state
+
+    # Update FSM variables with AI variables
+    X = update_X
+    Y = update_Y
+    ROI = update_ROI
+
+    # Find command from FSM
+    command = states.get(next_state)()
+
+    # Store AI variables (after they've been used) for use in next FSM_TICK call
+    PrevX = X
+    PrevY = Y
+    PrevROI = ROI
+
+    print ("PrevX: " + str(PrevX))
+    print ("PrevY: " + str(PrevY))
+    print ("PrevROI: " + str(PrevROI))
+
+    return command 
+
--- fsm/fsm_test.py
+++ fsm/fsm_test.py
@@ -1,134 +1,55 @@
-from pickle import TRUE
+#from expertFSM import INIT, TAKEOFF, LAND, IDLE, MOVE_LEFT, MOVE_RIGHT, MOVE_DOWN, MOVE_UP, MOVE_FORWARD, MOVE_BACKWARD, FSM_TICK, work, states
+#from expertFSM import *
+from random import randint
+import expertFSM
+# TO-Do: test cythonized version
+#from setup import ext_modules
+# Global variables for FSM drone operation
+#state = 1   #TO-DO: Remove state stuff from this. It should all be contained in expertFSM.pyx
+#next_state = 1
+
+# X = 100
+# Y = 0
+# ROI = 0
 
 
-INIT = 'INIT'
-TAKEOFF = 'TAKEOFF'
-LAND = 'LAND'
-IDLE = 'IDLE'
-MOVE_LEFT = 'MOVE_LEFT'
-MOVE_RIGHT = 'MOVE_RIGHT'
-MOVE_DOWN = 'MOVE_DOWN'
-MOVE_UP = 'MOVE_UP'
-MOVE_FORWARD = 'MOVE_FORWARD'
-MOVE_BACKWARD = 'MOVE_BACKWARD'
-X = 100
-PrevX = 0
-Y = 0
-PrevY = 0
-ROI = 0
-PrevROI = 0
-next_state = 1
+#command = "back 5" # arbitrary command to test if variable is updated correctly
 
-
-def INIT():
-    print("INIT STATE")
-    global next_state
-    next_state = 2 # move to TAKEOFF STATE
-    pass
-
-def TAKEOFF():
-    print("TAKEOFF_STATE")
-    global next_state
-    next_state = 4 # move to idle state
-    
-    pass
-
-def LAND():
-    print("LAND_STATE")
-    #state_switch(next_state)
-    pass
-
-def IDLE():
-    print("IDLE_STATE")
-    global next_state
-    if X < PrevX:
-        next_state = 5 #MOVE_LEFT #5
-    elif X > PrevX:
-        next_state = 6 #MOVE_RIGHT #6
-    elif ROI > PrevROI:
-        next_state = 10 # MOVE_BACKWARD #10
-    elif ROI < PrevROI:
-        next_state = 9 #MOVE_FORWARD #9
-    elif Y < PrevY:
-        next_state = 8 #MOVE_UP #8
-    elif Y > PrevY:
-        next_state = 7 #MOVE_DOWN #7
+# Function to test cythonized FSM   # equivalent to loop that calls FSM
+def droneCommand(X, Y, ROI): # may need to store PrevROI, PrevX, etc in this file if Cython loses the information after running
+    print ('in dronecommand \n')
+    command = str(expertFSM.FSM_TICK(X, Y, ROI))
+    if (command == 'null'): # check to see what is returned w/o assigning val to IDLE state
+        print (str(command) + 'rip\n')
+        pass # do nothing
     else:
-        next_state = 4
-        
-	#elif(battery < battery_shutoff):
-	#	next_state = FINAL
-    
-    pass
+        return command
 
-def MOVE_LEFT():
-    print("MOVE_LEFT_STATE")
-    global next_state
-    next_state = 4
-    
-    pass
+# Function to test dronecommand. Equivalent to using the value returned from loop in AI python file
+def testDroneCommand(X, Y, ROI):
+    ('in testdronecommand \n')
+    #expertFSM.state = 0
+    #expertFSM.startup()
+    i = 0
+    while(i < 100):
+        rand1 = randint(0,100)
+        X = rand1
+        rand2 = randint(0,100)
+        Y = rand2
+        print ("X: " + str(X))
+        print ("Y: " + str(Y))
 
-def MOVE_RIGHT():
-    print("MOVE_RIGHT_STATE")
-    global next_state
-    next_state = 4
-    
-    pass
+        print (droneCommand(X, Y, ROI))
+        i = i + 1
 
-def MOVE_DOWN():
-    print("MOVE_DOWN_STATE")
-    global next_state
-    next_state = 4
-    
-    pass
+# Calling to test
+print ('Starting... \n')
+X = 0
+Y = 0
+ROI = 0
+testDroneCommand(X, Y, ROI)
 
-def MOVE_UP():
-    print("MOVE_UP_STATE")
-    global next_state
-    next_state = 4
-    
-    pass
+#print(work())
+#TO-Do: loop dronecommand to test FSM
 
-def MOVE_FORWARD():
-    print("MOVE_FORWARD_STATE")
-    global next_state
-    next_state = 4
-    
-    pass
-
-def MOVE_BACKWARD():
-    print("MOVE_BACKWARD_STATE")
-    global next_state
-    next_state = 4
-    
-    pass  
-
-states = {
-    1: INIT, 
-    2: TAKEOFF, 
-    3: LAND, 
-    4: IDLE, 
-    5: MOVE_LEFT, 
-    6: MOVE_RIGHT,
-    7: MOVE_DOWN, 
-    8: MOVE_UP, 
-    9: MOVE_FORWARD, 
-    10: MOVE_BACKWARD
-    }
-
-def FSM_TICK(state): #FSM call probably
-    states.get(state)()
-
-
-def MAIN():
-    global X, PrevX, Y, PrevY, ROI, PrevROI,next_state
-    count = 0
-    while count < 100:
-        X = X - 1
-        PrevX = PrevX + 1
-        print("Count: " + str(count))
-        FSM_TICK(next_state)
-        count = count + 1
-
-MAIN()
 
