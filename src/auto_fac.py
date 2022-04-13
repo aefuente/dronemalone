db4d2f4b1cb3afe632224d338d3a89813ed85fe5
Andre Fuentes <andfnts@gmail.com>
2022-04-13 12:47:10 -0500

    refactor FrontEnd class

--- src/auto_fac.py
+++ src/auto_fac.py
@@ -25,3 +25,25 @@ FACE_CASCADE = cv2.CascadeClassifier('./src/cascades/haarcascade_frontalface_def
 
 CENTER_FRAME = (SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
 
+class FrontEnd(object):
+
+    def __init__(self):
+        self.for_back_velocity = 0
+        self.left_right_velocity = 0
+        self.up_down_velocity = 0
+        self.yaw_velocity = 0
+        self.speed = DRONE_SPEED
+
+        self.send_rc_control = False
+
+        self.pygame.init()
+        self.pygame.time.set_timer(self.pygame.USERVENT + 1, 1000//FPS)
+
+        self.drone = Tello()
+
+
+    
+    def display_window(self):
+        self.pygame.display.set_caption(WINDOW_DISPLAY_CAPTION)
+
+        self.screen = self.pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
