c7a03fe1f528dde3ed684746bab8b6207e3fba81
Andre Fuentes <andfnts@gmail.com>
2022-04-13 14:37:00 -0500

    creating controller

--- src/auto_fac.py
+++ src/auto_fac.py
@@ -56,10 +56,10 @@ class Drone:
 
 ''' 
 
-Class for intitializing window display
+Class for controlling the drone
 
 '''
-class FrontEnd:
+class Controller:
 
     def __init__(self):
         self.for_back_velocity = 0
@@ -67,14 +67,103 @@ class FrontEnd:
         self.up_down_velocity = 0
         self.yaw_velocity = 0
         self.speed = DRONE_SPEED
-
         self.send_rc_control = False
+        self.drone = Drone()
+    
+    def initialize_drone(self):
+        self.drone.connect()
 
-        self.pygame.init()
-        self.pygame.time.set_timer(self.pygame.USERVENT + 1, 1000//FPS)
+        self.drone.set_speed(DRONE_SPEED)
+
+        self.drone.refresh_stream()
+
+    def reset_velocity(self):
+        self.for_back_velocity = 0
+        self.left_right_velocity = 0
+        self.up_down_velocity = 0
+        self.yaw_velocity = 0
+    
+    def test_quit(self, event):
+        if event.type == pygame.QUIT:
+            return True
+        elif event.type == pygame.KEYDOWN:
+            return True
+        return False
+
+
+    def user_event(self, event,flag=True):
+
+        self.update()
+
+        if flag is True:
+            pygame.time.wait(500)
+
+        self.reset_velocity()
+        self.update()
+
+
+
+    def run(self):
+        should_stop = False
+        flag = False
+
+        while not should_stop:
+            for event in pygame.event.get():
+
+                if event.type == pygame.USEREVENT + 1:
+                    self.user_event(event,flag)
+                    flag=False
+
+                elif event.type == pygame.KEYUP:
+                    self.keyup(event.key)
+
+                should_stop = self.test_quit(event)
+
+
+
+
+    def update(self):
+        if self.send_rc_control:
+            self.drone.send_rc_control(self.left_right_velocity, \
+                                  self.for_back_velocity,   \
+                                  self.up_down_velocity,    \
+                                  self.yaw_velocity)
+
+
+
+    def keyup(self, key):
+        if key == pygame.K_t:  # takeoff
+            self.drone.takeoff()
+            self.send_rc_control = True
+        elif key == pygame.K_l:  # land
+            self.drone.land()
+            self.send_rc_control = False
 
     
-    def display_window(self):
-        self.pygame.display.set_caption(WINDOW_DISPLAY_CAPTION)
-        self.screen = self.pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 
+
+
+''' Function for initializing Pygame '''
+def intialize_pygame():
+    pygame.init()
+    pygame.time.set_timer(pygame.USERVENT + 1, 1000//FPS)
+
+
+
+''' Function for displaying pygame window '''
+def display_window():
+    pygame.display.set_caption(WINDOW_DISPLAY_CAPTION)
+    return pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+
+
+
+
+if __name__ == '__main__':
+
+
+    controller = Controller()
+    controller.initialize_drone()
+
+    frame = controller.drone.get_frame()
+
+ 
